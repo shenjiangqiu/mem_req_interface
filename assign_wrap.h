08a05e650b475a016c64e387b0c25a04b8ee382a
Jiangqiu Shen <jshen2@mtu.edu>
2020-09-07 04:40:01 -0400

    add new interface

--- assign_wrap.h
+++ assign_wrap.h
@@ -13,24 +13,29 @@ public:
     }
     bool is_read_clause(int index)
     {
-        return modified_clause_list_items.find(index) != modified_clause_list_items.end();
+        return clause_addr.find(index) != clause_addr.end();
     }
     assign_wrap() = delete;
     void add_pushed_list(int index, int value) { pushed_other_list_items.insert(std::make_pair(index, value)); }
-    void add_modified_list(int index, unsigned long long value) { modified_clause_list_items.insert(std::make_pair(index, value)); }
+    void add_pushed_addr(int index, unsigned long long addr) { pushed_other_list_addr.insert(std::make_pair(index, addr)); }
+    void add_clause_addr(int index, unsigned long long value) { clause_addr.insert(std::make_pair(index, value)); }
     void add_detail(int index, unsigned long long value) { clause_detail[index].push_back(value); }
     const std::vector<unsigned long long> &get_clause_detail(int index) const { return clause_detail.at(index); }
     void set_watcher_size(int size) { watcher_size = size; }
     int get_watcher_size() const { return watcher_size; }
     auto get_pushed(int index) const { return pushed_other_list_items.at(index); }
+    auto get_pushed_watcher_list_tail_addr(int index)
+    {
+        return pushed_other_list_addr.at(index);
+    }
     auto get_is_push_to_other(int index) const
     {
         return pushed_other_list_items.find(index) != pushed_other_list_items.end();
     }
     auto get_modified_by_range(int start, int end) const
     {
-        auto start_iter = modified_clause_list_items.lower_bound(start);
-        auto end_iter = modified_clause_list_items.lower_bound(end);
+        auto start_iter = clause_addr.lower_bound(start);
+        auto end_iter = clause_addr.lower_bound(end);
         return std::make_pair(start_iter, end_iter);
     }
     unsigned int get_level() const { return level; }
@@ -65,10 +70,10 @@ public:
     void set_addr(unsigned long long t_addr)
     {
         addr = t_addr;
-    }                                                                                                    //watcher list addr
-    unsigned long long get_addr() const { return addr; }                                                 //watcher list addr
-    unsigned long long get_clause_addr(int index) const { return modified_clause_list_items.at(index); } //clause addr
-    void add_clause_literal(int index, int literal) { clause_literals[index].push_back(literal); }       // for read
+    }                                                                                              //watcher list addr
+    unsigned long long get_addr() const { return addr; }                                           //watcher list addr
+    unsigned long long get_clause_addr(int index) const { return clause_addr.at(index); }          //clause addr
+    void add_clause_literal(int index, int literal) { clause_literals[index].push_back(literal); } // for read
     auto &get_clause_literal(int index) { return clause_literals[index]; }
     void add_block_addr(int index, uint64_t addr)
     {
@@ -83,19 +88,19 @@ public:
 private:
     std::map<int, uint64_t> blocker_value_addr;
 
-    unsigned long long clause_addr;
+    //unsigned long long clause_addr;
     int value;
     int watcher_size;
     int depend_id;
     assign_wrap *depend_value;
     //std::pair<int, assign_wrap*> Depends;
-    std::map<int, unsigned long long> modified_clause_list_items;
+    std::map<int, unsigned long long> clause_addr;
     std::map<int, std::vector<unsigned long long>> clause_detail;
     std::map<int, int> pushed_other_list_items;
     int generated_conf;
     std::map<int, std::vector<int>> clause_literals;
-    std::map<int, assign_wrap *> generated_assignments; // must make the ptr weak here to break circular;
-
+    std::map<int, assign_wrap *> generated_assignments;       // must make the ptr weak here to break circular;
+    std::map<int, unsigned long long> pushed_other_list_addr; //the addr of the pushed watcher list
     int clause_size;
 
     unsigned level;
